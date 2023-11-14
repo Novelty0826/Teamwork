7d19e36be9f42c9cb312af64550d55d83c9cf6fd
linmercury <144221750+linmercury@users.noreply.github.com>
2023-11-14 20:52:12 +0800

    Add files via upload

---
+++ 102101308/dljm/dljm.js
@@ -0,0 +1,93 @@
+// pages/dljm/dljm.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        msg:'',
+        msg2:'',
+
+    },
+    //输入框的事件处理函数
+    inputhandler(e){
+        //console.log(e.detail.value)
+        this.setData({
+            msg:e.detail.value
+        })
+    },
+    inputhandler2(e){
+        //console.log(e.detail.value)
+        this.setData({
+            msg2:e.detail.value
+        })
+    },
+    jump1:function(){
+        console.log('dianl')
+        wx.navigateTo({
+          url: '/pages/kcy/kcy',
+        })
+    },
+    jump2:function(){
+        console.log('dianl')
+        wx.navigateTo({
+          url: '/pages/zcjm/zcjm',
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
+})
